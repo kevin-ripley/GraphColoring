fa627fd9eb3b72baa991d332ea07e8ed3f0760bb
Bryan Downs <xerobasix@gmail.com>
2016-09-19 09:52:24 -0600

    Added comment layout to GA and MinConflicts

---
+++ GraphColoring/src/graphcoloring/MinConflicts.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphcoloring;
+
+/**
+ *
+ * @author bryan
+ */
+public class MinConflicts {
+    
+    /**
+     * Constructor
+     * 
+     * @param graph A randomly and completely colored graph (type ArrayList<vertex>?)
+     * @param max_attempts integer maximum number of iterations through the algorithm
+     */
+    
+    /**
+     * Find Solution Method
+     */
+    
+    /**
+     * Get Max Attempts Method
+     * @return the current value for maximum attempts
+     */
+    
+    /**
+     * Set Max Attempts Method
+     * @param an integer for the new maximum attempts for the algorithm
+     */
+    
+}
